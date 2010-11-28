535ccd06a10893aff5c39ff0774e89cc3e140418
cbp10_uschok <cbp10_uschok@a60ba226-48e3-df11-83e3-000476a39db3>
2010-11-28 22:37:23 +0000

    Implemented LT, GT, LE and GE
    git-svn-id: https://elmar.ips.cs.tu-bs.de/svn/cbp_ws1011@146 a60ba226-48e3-df11-83e3-000476a39db3

--- codegen/src/ExprNodes.cpp
+++ codegen/src/ExprNodes.cpp
@@ -77,15 +77,71 @@ void Expr_NEQ::gen(CodeGen* out) {
 }
 
 void Expr_LT::gen(CodeGen* out) {
+    // left hand operand into %eax
+    left->gen(out);
+    // save to local var
+    *out << "pushl  %ebx" << std::endl;
+    *out << "movl %eax, %ebx" << std::endl;
+    // right operand into %eax
+    right->gen(out);
+    // compare
+    *out << "cmpl %eax, %ebx" << std::endl;
+    // set LSB of %ax if less than
+    *out << "setl %al" << std::endl;
+    // move from byte %al to long %eax and pad with zeros
+    *out << "movzbl %al, %eax" << std::endl
+    *out << "pop %ebx" << std::endl;
 }
 
 void Expr_GT::gen(CodeGen* out) {
+    // left hand operand into %eax
+    left->gen(out);
+    // save to local var
+    *out << "pushl  %ebx" << std::endl;
+    *out << "movl %eax, %ebx" << std::endl;
+    // right operand into %eax
+    right->gen(out);
+    // compare
+    *out << "cmpl %eax, %ebx" << std::endl;
+    // set LSB of %ax if greater than
+    *out << "setg %al" << std::endl;
+    // move from byte %al to long %eax and pad with zeros
+    *out << "movzbl %al, %eax" << std::endl
+    *out << "pop %ebx" << std::endl;
 }
 
 void Expr_LE::gen(CodeGen* out) {
+    // left hand operand into %eax
+    left->gen(out);
+    // save to local var
+    *out << "pushl  %ebx" << std::endl;
+    *out << "movl %eax, %ebx" << std::endl;
+    // right operand into %eax
+    right->gen(out);
+    // compare
+    *out << "cmpl %eax, %ebx" << std::endl;
+    // set LSB of %ax if less than or equal
+    *out << "setle %al" << std::endl;
+    // move from byte %al to long %eax and pad with zeros
+    *out << "movzbl %al, %eax" << std::endl
+    *out << "pop %ebx" << std::endl;
 }
 
 void Expr_GE::gen(CodeGen* out) {
+    // left hand operand into %eax
+    left->gen(out);
+    // save to local var
+    *out << "pushl  %ebx" << std::endl;
+    *out << "movl %eax, %ebx" << std::endl;
+    // right operand into %eax
+    right->gen(out);
+    // compare
+    *out << "cmpl %eax, %ebx" << std::endl;
+    // set LSB of %ax if greater than or equal
+    *out << "setge %al" << std::endl;
+    // move from byte %al to long %eax and pad with zeros
+    *out << "movzbl %al, %eax" << std::endl
+    *out << "pop %ebx" << std::endl;
 }
 
 void Expr_BoolOR::gen(CodeGen* out) {
