54662bbfc1ba1559576223acc7d8cc1a2f421cc4
cbp10_schmidt <cbp10_schmidt@a60ba226-48e3-df11-83e3-000476a39db3>
2010-12-03 11:52:48 +0000

    FunctionNodes: We use spaces and not tabs here.
    Dum di dum, keep walking, nothing to see here...
    
    git-svn-id: https://elmar.ips.cs.tu-bs.de/svn/cbp_ws1011@185 a60ba226-48e3-df11-83e3-000476a39db3

--- codegen/src/FunctionNodes.cpp
+++ codegen/src/FunctionNodes.cpp
@@ -8,7 +8,7 @@ void Function::gen(CodeGen* out) {
     *out << mark << ":" << endl;
 
     if (identifier == "main")
-	*out << "__cbp_main:" << endl;
+        *out << "__cbp_main:" << endl;
 
     if(statement)
         statement->gen(out);
