4dc8c25dc18118dbd6542ec3184844432e12008a
cbp10_schmidt <cbp10_schmidt@a60ba226-48e3-df11-83e3-000476a39db3>
2011-01-31 13:15:50 +0000

    Peephole: Add simple search and replace function
    git-svn-id: https://elmar.ips.cs.tu-bs.de/svn/cbp_ws1011@364 a60ba226-48e3-df11-83e3-000476a39db3

--- optimizer/src/Peephole.cpp
+++ optimizer/src/Peephole.cpp
@@ -3,6 +3,22 @@
 #include <string>
 #include <iostream>
 
+// Simple search and replace
+std::string searchReplace(const std::string &SearchString, const std::string &ReplaceString, std::string String)
+{
+        std::string::size_type pos = String.find(SearchString, 0);
+        int LengthSearch = SearchString.length();
+        int LengthReplace = ReplaceString.length();
+
+        while(std::string::npos != pos)
+        {
+                String.replace(pos, LengthSearch, ReplaceString);
+                pos = String.find(SearchString, pos + LengthReplace);
+        }
+
+        return String;
+}
+
 void backupFile(FILE *file) {
 
     char buffer[4096];
@@ -31,7 +47,6 @@ void optimizePeephole(std::string fname) {
 
     // Open generated assembler output file read-write to optimize in-place
     file = fopen(fname.c_str(), "r+");
-
     if(file==NULL)
         std::cout << " -error: file not found " << fname << std::endl;
 
