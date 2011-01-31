e61cce790b8ad03c630547de9df37eb579520383
cbp10_basold <cbp10_basold@a60ba226-48e3-df11-83e3-000476a39db3>
2011-01-31 18:02:51 +0000

    Comments in Interval
    git-svn-id: https://elmar.ips.cs.tu-bs.de/svn/cbp_ws1011@376 a60ba226-48e3-df11-83e3-000476a39db3

--- include/optimizer/Intervals.h
+++ include/optimizer/Intervals.h
@@ -1,18 +1,20 @@
 #ifndef INTERVALS_H
 #define INTERVALS_H
 
-
 #include <ostream>
 
 class Interval {
 
 public:
+    // construct ∅
     Interval()
         : low(1), up(0) {}
         
+   // construct [a,a]
    Interval(int val)
         : low(val), up(val) {}
 
+    // construct [a,b]
     Interval(int low_, int up_)
         : low(low_), up(up_) {}
     
@@ -21,21 +23,26 @@ public:
     // [a,b] = [⌊a⌋, ⌈b⌉]
     Interval(double val);
     Interval(double low_, double up_);
-        
+    
+    // construct [-∞,+∞]
+    static Interval world();
+    
+    // [a,b] == ∅ ?
     bool empty() const {
         return up < low;
     }
     
+    // return a from [a,b]
     int lower() const {
         return low;
     }
     
+    // return b from [a,b]
     int upper() const {
         return up;
     }
     
-    static Interval world();
-    
+    // [a,b] == [c,d] ?
     bool operator==(Interval const& rhs) const {
         return lower() == rhs.lower() && upper() == rhs.upper();
     }
@@ -45,11 +52,13 @@ private:
     int up;
 };
 
+// interval arithmetic
 Interval operator+ (Interval const& lhs, Interval const& rhs);
 Interval operator- (Interval const& lhs, Interval const& rhs);
 Interval operator* (Interval const& lhs, Interval const& rhs);
 Interval operator/ (Interval const& lhs, Interval const& rhs);
 
+// I/O
 std::ostream& operator<<(std::ostream& os, Interval const& i);
 
 #endif // INTERVALS_H
